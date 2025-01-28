1d5c5e5a351b8c53d0e0181715042d688d767b8d
Kawan Silva <99221604+ksilva-kwn@users.noreply.github.com>
2025-01-27 22:33:39 -0300

    Create condicionais.sh

---
+++ Introdução ao Shell Script/condicionais.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+echo "Condição 1, onde as duas variáveis são iguais"
+
+VAR="1"
+VAR_2="1"
+
+if [[ "$VAR" = "$VAR_2" ]]; then
+        echo "Os números são iguais."
+else
+        echo "Não são iguais."
+
+fi
+
+## Maneira diferente de construir a condição
+[ "$VAR" = "$VAR_2" ] && echo "Os números são iguais."
+
+echo "----------------------------------------------------------------"
+
+echo "Condição 2, onde as duas variáveis não são iguais"
+
+VAR_3="3"
+VAR_4="4"
+
+if [[ "$VAR_3" = "$VAR_4" ]]; then
+        echo "Os números são iguais."
+else
+        echo "Os números não são iguais."
+
+fi
